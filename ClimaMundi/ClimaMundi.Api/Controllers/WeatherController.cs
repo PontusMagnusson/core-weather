c58696e14792e7c1f75d1fe102b1c68184a38140
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-05-28 15:06:39 +0200

    Add endpoint documentation

--- ClimaMundi/ClimaMundi.Api/Controllers/WeatherController.cs
+++ ClimaMundi/ClimaMundi.Api/Controllers/WeatherController.cs
@@ -20,13 +20,23 @@ namespace ClimaMundi.Api.Controllers
             _weatherService = new WeatherService(apiKey); // Make this an interface later
         }
 
-
+        /// <summary>
+        /// Get the weather for a location specified by a coordinate pair
+        /// </summary>
+        /// <param name="latitude">The positions latitude</param>
+        /// <param name="longitude">The positions longitude</param>
+        /// <returns></returns>
         [HttpGet("{latitude},{longitude}")]
         public async Task<IActionResult> GetByCoordinates(double latitude, double longitude)
         {
             return Ok(await _weatherService.GetWeatherByCoordinates(latitude, longitude));
         }
 
+        /// <summary>
+        /// Get the weather for a location by a text search, picks the most relevant location based on the search.
+        /// </summary>
+        /// <param name="location">The location name</param>
+        /// <returns></returns>
         [HttpGet("{location}")]
         public async Task<IActionResult> GetByLocation(string location)
         {
